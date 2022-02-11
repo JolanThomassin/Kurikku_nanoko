9cdc8f3251bc17175a4c423715e6612afab410d8
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2022-02-11 03:39:34 +0100

    Kurikku Nanoko version 1

---
+++ Kurikku nanoko v1/stockage/timeur.py
@@ -0,0 +1,24 @@
+# Skin Légendaire - 1/100
+# Skin Épique - 9/100
+# Skin Rare - 35/100
+# Skin Commun - 55/100
+# -----------------
+# Skin Légendaire - 1%
+# Skin Épique - 9%
+# Skin Rare - 35%
+# Skin Commun - 55%
+# ----------------- 
+# Lunette - 10%
+# Casquette - 17%
+# Boucle d'oreille - 15%
+# Masque - 13%
+# Yeux rouge - 21%
+# Shiba - 14%
+# Tail - 9% 
+# Légendaire - 1%
+
+LunetteCommunPossession = False
+
+if LunetteCommunPossession == False :
+  print(bool("\n"))
+
